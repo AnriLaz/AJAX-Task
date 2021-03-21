4423a4c133d3ec57c5beb76c08a6a3e4b0c72c2e
Anri <anri.lazashvili@georgiatech.eu>
2021-03-22 00:42:19 +0400

    feat: json parse

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
-//on click function
-//for info load
+// JSON
+// Java Script Object Notation
+//AJAX
+//Asynchronous Javascript And XML
+const myRequest = new XMLHttpRequest();
+myRequest.open('GET','https://jsonplaceholder.typicode.com/photos');
+myRequest.onload = function (){
+    const myData = JSON.parse(myRequest.responseText);
+    console.log(myData[0]);
+};
+myRequest.send();
