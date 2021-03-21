72cdb502b68162ee35e069670a040211d37c6a9e
Anri <anri.lazashvili@georgiatech.eu>
2021-03-21 23:43:13 +0400

    deleted stupid shi..

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 //on click function
 //for info load
-function loadInfo(){
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function (){
-       if (xhttp.readyState == 4 && xhttp.status == 200){
-          document.getElementById('demo').innerHTML = xhttp.response;
-       }
-    };
-    xhttp.open("GET","https://jsonplaceholder.typicode.com/photos")
-    xhttp.send();
-}
