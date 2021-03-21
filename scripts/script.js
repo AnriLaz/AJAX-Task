83b62bd43fde3eaef44286c1b0aee6d2696a0a77
Anri <anri.lazashvili@georgiatech.eu>
2021-03-22 00:49:38 +0400

    feat: button and event listener

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,13 @@
 // Java Script Object Notation
 //AJAX
 //Asynchronous Javascript And XML
-const myRequest = new XMLHttpRequest();
-myRequest.open('GET','https://jsonplaceholder.typicode.com/photos');
-myRequest.onload = function (){
-    const myData = JSON.parse(myRequest.responseText);
-    console.log(myData[0]);
-};
-myRequest.send();
+const button = document.getElementById('button');
+button.addEventListener('click',function (){
+    const myRequest = new XMLHttpRequest();
+    myRequest.open('GET','https://jsonplaceholder.typicode.com/photos');
+    myRequest.onload = function (){
+        const myData = JSON.parse(myRequest.responseText);
+        
+    };
+    myRequest.send();
+});
